b6cc61aebc9f3a77ed499411675185d49956fcdc
Roull Joseph Hapil <rhapil@umpisa.co>
2022-02-02 04:35:52 +0000

    Change exception for invalid user

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
