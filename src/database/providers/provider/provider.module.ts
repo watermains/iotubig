8b83720c2b2317de0e6038e5ed83e22858f91989
Sammy Medina <sammymedina@Appers-MacBook-Air.local>
2022-02-22 14:00:37 +0800

    Update DB connection options

--- src/database/providers/provider/provider.module.ts
+++ src/database/providers/provider/provider.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
+var key = require('fs').readFileSync('rds-combined-ca-bundle.pem');
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL, {
       ssl: true,
-      sslCA: `${__dirname}/rds-combined-ca-bundle.pem`
+      sslValidate: true,
+      sslCA: key
     }),
   ],
 })
