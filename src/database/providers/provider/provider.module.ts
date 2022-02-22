bea836d536daa997fa698e4d1c69fb0385aeeabd
Sammy Medina <sammymedina@Appers-MacBook-Air.local>
2022-02-22 17:18:51 +0800

    Update SSL config

--- src/database/providers/provider/provider.module.ts
+++ src/database/providers/provider/provider.module.ts
@@ -2,16 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
-console.log(process.env.MONGO_URL);
-console.log(__dirname + '/rds-combined-ca-bundle.pem');
-
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL, {
       ssl: true,
       sslValidate: true,
-      sslCA: __dirname + '/rds-combined-ca-bundle.pem',
+      sslCA: 'rds-combined-ca-bundle.pem',
       useNewUrlParser: true
     }),
   ],
