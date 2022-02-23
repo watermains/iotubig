4735e26913b173c78e4481ea675a3c2fdab2c46b
Sammy Medina <sammymedina@Appers-MacBook-Air.local>
2022-02-23 20:06:49 +0800

    Change bcrypt to bcryptjs

--- src/module/user/customer/user.service.ts
+++ src/module/user/customer/user.service.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRepository } from '../user.repository';
