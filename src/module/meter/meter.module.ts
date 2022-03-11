51717eea7292d4dfe6d7d9a5ffa9db4bf9d8a682
dennis <dennis@umpisa.co>
2022-03-11 14:21:42 +0800

    Cleaned imports in Meters

--- src/module/meter/meter.module.ts
+++ src/module/meter/meter.module.ts
@@ -1,18 +1,17 @@
 import { HttpModule } from '@nestjs/axios';
-import { forwardRef, MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import {
+  forwardRef,
+  MiddlewareConsumer,
+  Module,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { IotService } from 'src/iot/iot.service';
 import { APIKeyMiddleware } from 'src/middleware/apikey.middleware';
 import { MeterCheckConstraint } from 'src/validators/meter.validator';
 import { ConfigurationModule } from '../configuration/configuration.module';
-import {
-  Configuration,
-  ConfigurationSchema,
-} from '../configuration/entities/configuration.schema';
 import { ScreenerModule } from '../screener/screener.module';
-import { User, UserSchema } from '../user/entities/user.schema';
 import { UserModule } from '../user/user.module';
 import { Meter, MeterSchema } from './entities/meter.schema';
 import { ExternalMeterController, MeterController } from './meter.controller';
