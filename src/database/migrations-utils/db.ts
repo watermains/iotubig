df41b16fe03e4ede49960c1856d99e5dd9713f21
dennis <dennis@umpisa.co>
2022-03-16 15:30:01 +0800

    Added Empty Migration

--- src/database/migrations-utils/db.ts
+++ src/database/migrations-utils/db.ts
@@ -1,10 +1,16 @@
 import { ConfigModule } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+import { Mongoose } from 'mongoose';
 
 export const getDb = async () => {
   ConfigModule.forRoot();
-  const client: MongoClient = await MongoClient.connect(process.env.MONGO_URL, {
+  const mongoose = new Mongoose();
+  mongoose.connect(process.env.MONGO_URL, {
     directConnection: true,
   });
-  return client.db();
+  // const client: MongoClient = await MongoClient.connect(process.env.MONGO_URL, {
+  //   directConnection: true,
+  // });
+  // return client.db();
+  return mongoose;
 };
