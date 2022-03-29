bb1554c4a30014ef253b2baf191682ff721206dd
Ivan Gayares <ivan@umpisa.co>
2022-03-29 08:08:51 +0000

    HOT FIX - Cron at 1630H

--- src/cron/balance.check.ts
+++ src/cron/balance.check.ts
@@ -35,7 +35,7 @@ export class BalanceCheckService {
   @Cron(
     process.env.NODE_ENV === 'development'
       ? '*/10 * * * * *'
-      : '00 00 15 * * *',
+      : '00 30 16 * * *',
     // : CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT,
     {
       timeZone: 'Asia/Manila',
