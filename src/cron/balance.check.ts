e1d2c81ff4685d1f0b7c95d9757ca873cba1496a
Ivan Gayares <ivan@umpisa.co>
2022-03-29 09:14:44 +0000

    HOT FIX - test cron and minimum zero

--- src/cron/balance.check.ts
+++ src/cron/balance.check.ts
@@ -35,7 +35,7 @@ export class BalanceCheckService {
   @Cron(
     process.env.NODE_ENV === 'development'
       ? '*/10 * * * * *'
-      : '00 02 17 * * *',
+      : '00 28 17 * * *',
     // : CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT,
     {
       timeZone: 'Asia/Manila',
