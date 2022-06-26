7d3b9d7c0ae6bde86b56202c4f3ebb4f7c673156
Kevin Luis <kevluis10@gmail.com>
2022-06-26 18:21:38 +0800

    add new user test

--- src/database/seeders/admin/admin.service.ts
+++ src/database/seeders/admin/admin.service.ts
@@ -8,14 +8,15 @@ export class AdminSeederService {
     const admins = require('./admin.json');
     const data = [];
     admins.forEach((admin) => {
-      if (
-        admin.role == 'super_admin' 
-      ) {
+      // if (
+      //   admin.role == 'super_admin' 
+      // ) {
         admin.organization_id = organization._id;
-      }
+      // }
       data.push(admin);
     });
     const res = Array<Promise<unknown>>();
+    // Promise.resolve(this.userRepository.seedAdmin(data))
     res.push(...this.userRepository.seedAdmin(data));
     return res;
   }
