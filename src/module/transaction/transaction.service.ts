b798b637fd20b7919586291d3c7f70e840bd0d37
Kevin Luis <kevluis10@gmail.com>
2023-01-23 11:53:27 +0800

    new build checking

--- src/module/transaction/transaction.service.ts
+++ src/module/transaction/transaction.service.ts
@@ -386,6 +386,7 @@ export class TransactionService {
     const btoa64 = btoa(process.env.XENDIT_SECRET);
     const channel_code = TransactionPaymentOptions[dto.payment_channel];
     console.log('the token: ', process.env.XENDIT_SECRET);
+    console.log('the url: ', process.env.XENDIT_URL);
     const response = this.httpService
       .post(
         `${process.env.XENDIT_URL}/ewallets/charges`,
